fcacbcf9850cbf622ccdaa9ad366af5f98b4283f
Zou Guangxian <zouguangxian@gmail.com>
2025-01-22 09:00:03 +0000

    chore: add more utils functions

--- packages/utils/src/utils.ts
+++ packages/utils/src/utils.ts
@@ -1,7 +1,19 @@
-import * as fs from "fs";
+import fs from "fs";
+import * as path from 'path';
+import * as process from 'process';
+
 export function readFile(filename: string): string {
   return fs.readFileSync(filename, "utf-8");
 }
+
 export function hello(): string {
   return "Hello, world!";
+}
+
+export function abspath(filename: string):string {
+  return path.resolve(filename);
+}
+
+export function cwd():string {
+  return process.cwd();
 }
