0621de58ce92488b75dcdba8c249ce8e2c0b27a8
Nazreen <nxmohamad@gmail.com>
2025-01-24 09:15:00 +0000

    stash

--- apps/nextjs-app/app/page.tsx
+++ apps/nextjs-app/app/page.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import Image from "next/image";
-import {hello} from '@example/utils';
+import * as oftSdk from "@layerzerolabs/oft-v2-solana-sdk";
 
 export default function Home() {
-  hello();
+  // hello();
+  console.log(oftSdk);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
--- apps/vite-project/vite.config.ts
+++ apps/vite-project/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import nodePolyfills from 'vite-plugin-node-stdlib-browser'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react(), nodePolyfills()]
+  ,
 })
